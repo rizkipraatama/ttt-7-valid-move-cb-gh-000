58b5605c28c72dc7f61d261d81fcf8d6db2c9f44
Rizki Pratama <rizkipratama0602@gmail.com>
2019-08-18 06:45:11 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board, input)
-  if input > 9 || input < 1
+  if input > 8 || input < 0
     return false
   elsif position_taken?(board, input) == true
     return false
